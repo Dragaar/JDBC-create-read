d1b2ad7128dfd5dcd50b67c72010f13234e72910
Dragaar <rosivanyshyn@gmail.com>
2022-11-06 13:38:12 +0200

    Add Entity

--- src/main/java/com/epam/rd/java/basic/topic07/task01/db/entity/Team.java
+++ src/main/java/com/epam/rd/java/basic/topic07/task01/db/entity/Team.java
@@ -1,9 +1,36 @@
 package com.epam.rd.java.basic.topic07.task01.db.entity;
 
+import java.util.Objects;
+
 public class Team {
 
 	private int id;
 		
 	private String name;
 
+	private Team(String name){ this.name = name; id = 0;}
+	static Team createTeam(String name){
+		return new Team (name);
+	}
+	public String getName() { return name; }
+
+	@Override
+	public String toString() {
+		return "Team{" +
+				"name='" + name + '\'' +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Team team = (Team) o;
+		return name.equals(team.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
 }
--- src/main/java/com/epam/rd/java/basic/topic07/task01/db/entity/User.java
+++ src/main/java/com/epam/rd/java/basic/topic07/task01/db/entity/User.java
@@ -1,9 +1,37 @@
 package com.epam.rd.java.basic.topic07.task01.db.entity;
 
+import java.util.Objects;
+
 public class User {
 
 	private int id;
 
 	private String login;
 
+	private User(String login){ this.login = login; id = 0;}
+	static User createUser(String login){
+		return new User(login);
+	}
+	public String getLogin() { return login; }
+
+
+	@Override
+	public String toString() {
+		return "User{" +
+				"login='" + login + '\'' +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		User user = (User) o;
+		return login.equals(user.login);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(login);
+	}
 }
